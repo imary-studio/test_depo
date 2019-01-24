3f9d45f61cb2b16239ecde2cb743b2535b6df6c8
blosterr <blosterr@users.noreply.github.com>
2019-01-25 01:36:18 +0200

    4
    Mobile and tablet version + bug fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,28 @@ $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
     loop: true,
     autoplay: true,
-    autoHeight: true,
-    margin: 45,
-    autoWidth:false,
-    items: 3,
+    autoHeight: false,
     onInitialized: startProgressBar,
     onTranslate: resetProgressBar,
-    onTranslated: startProgressBar
+    onTranslated: startProgressBar,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+        margin: 0,
+        center: true,
+        stagePadding: 0,
+        autoWidth: false,
+      },
+      769: {
+        items: 3,
+        margin: 45,
+        autoWidth: false,
+      }
+    }
   });
 
   function startProgressBar() {
-    // apply keyframe animation
     $(".slide-progress").css({
       width: "1200px",
       transition: "width 5000ms"
@@ -25,6 +36,33 @@ $(document).ready(function(){
       transition: "width 0s"
     });
   }
+
+var navMenu = $("#menu");
+
+  $( ".mobile-icon" ).click(function() {
+if ($(navMenu).is(":hidden")) {
+  $(navMenu).slideDown(200);
+}
+  });
+
+
+$(".dropdown-item").click(function(){
+ $(this).next().slideToggle(200);
+})
+
+
+
+
+  $(document).mouseup(function (e){
+    if ( $(window).width() <= 768 ) {
+      if (navMenu.has(e.target).length === 0) {
+          navMenu.slideUp(200);
+      }
+  }
+
+  });
+
+
 
 
 });
